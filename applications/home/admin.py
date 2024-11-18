5bcc6e021e925b7b6edd1a1212d535251bece3f8
juanmagogof1 <40185268+juanmagogof1@users.noreply.github.com>
2024-11-18 11:08:01 -0600

    Admin Django_1

--- applications/home/admin.py
+++ applications/home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+#importar modelo
+from .models import Edades
 # Register your models here.
+#codigo para interactuar con base de datos
+
+admin.register(Edades)
+
+
+#para que se pueda visualizar el usuario debemos generar un nuevo usuario:
+#python manage.py createsuperuser
+#:user
+#:pass
--- applications/home/models.py
+++ applications/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Edades(models.Model):
+	nombre = models.CharField(max_length = 30)
+	apellidos = models.CharField(max_length = 50)
+	edad = models.IntegerField()
+
+#revisar si hay algun cambio en los modelos: python manage.py makemigrations
+#crear tabla en BD conectada: python manage.py migrate
